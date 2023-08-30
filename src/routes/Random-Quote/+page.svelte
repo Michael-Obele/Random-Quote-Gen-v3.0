7019176d12b4872048109bde775471d45139ccbd
Michael Obubelebra Amachree <amachree9630@gmail.com>
2023-08-30 17:17:11 +0100

    Fix typo in code

--- src/routes/Random-Quote/+page.svelte
+++ src/routes/Random-Quote/+page.svelte
@@ -25,22 +25,20 @@
 <button />
 
 {#await promise}
-  <div class="px-4 py-6">
+  <div
+    class=" bg-gray-800 shadow-lg rounded-lg mx-auto my-5 max-w-md md:max-w-2xl"
+  >
+    <div class="px-4 py-6">
       <div class="items-center relative">
-        <h2 class="text-lg font-semibold text-white -mt-1">
-          ----
-        </h2>
-        <small class=" absolute right-0 text-sm text-white">{text.time}</small>
+        <h2 class="text-lg font-semibold text-white -mt-1">Loading</h2>
+        <small class=" absolute right-0 text-sm text-white">Loading</small>
       </div>
-      <p class="text-white">Tag: {text.tags}</p>
-      <p class="mt-3 text-white text-sm">
-        ---
-      </p>
+      <p class="text-white">Tag: loading</p>
+      <p class="mt-3 text-white text-sm">Loading</p>
       <div class=" text-center mx-auto py-2">
-        <button on:click={handleClick} class=" bg-amber-400 w-2/4 rounded-lg"
+        <button class=" bg-amber-400 w-2/4 rounded-lg"
           >generating
           <i class="fa-solid fa-rotate fa-spin" />
-          <!-- <i class="fa-solid fa-rotate fa-spin" /> -->
         </button>
       </div>
     </div>
