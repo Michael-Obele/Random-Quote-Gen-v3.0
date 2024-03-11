44ef12559a312fc9992aa625c2fe299b8ca24568
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-03-11 18:46:18 +0100

    Added actives class to sm search button

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -76,6 +76,7 @@
     <div class="flex md:order-2">
       <a
         href="/Search"
+        class:actives={$page.url.pathname === '/Search'}
         class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
       >
         <i class="fa-solid fa-magnifying-glass fa-2x" />
